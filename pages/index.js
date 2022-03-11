4718cbd49c558070cac2433b73fa14b2b3555c03
purandar55 <85347661+purandar55@users.noreply.github.com>
2022-03-11 12:09:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+<head>
       <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
